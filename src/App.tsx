0e26fec75e29a3ebad775bc3c40506d7a575c883
sorashin <naka52shin@gmail.com>
2023-09-20 09:43:30 +0900

    enable path alias

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Scene from './components/organisms/Scene'
+import Scene from '@/components/organisms/Scene'
 
 function App() {
   const [count, setCount] = useState(0)
