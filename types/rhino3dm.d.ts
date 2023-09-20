94e85b2db1f91fcef61456795e68ac44afd2b981
sorashin <naka52shin@gmail.com>
2023-09-20 10:43:39 +0900

    fix error

---
+++ types/rhino3dm.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// types/rhino3dm.d.ts
+
+declare module 'rhino3dm' {
+    // ここに必要な型やインターフェースを定義します。
+    export type RhinoModule = any;
+    export type Sphere = any;
+    export type File3dm = any;
+    // 他の必要な型や関数もここに追加できます。
+  }
